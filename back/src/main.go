755b6e2ba4654daf87b859aecdbd4277e6e7529d
Yoqub <yoqub.dj03@gmail.com>
2023-08-19 21:14:35 +0300

    finished get requests

--- back/src/main.go
+++ back/src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -21,10 +23,23 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTask(w http.ResponseWriter, r *http.Request) {
-
+	taskID := mux.Vars(r)
+	flag := false
+	for i := 0; i < len(tasks); i++ {
+		if taskID["id"] == tasks[i].ID {
+			json.NewEncoder(w).Encode(tasks[i])
+			flag = true
+			break
+		}
+	}
+	if !flag {
+		json.NewEncoder(w).Encode(map[string]string{"status" : "Error"})
+	}
 }
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func createTask(w http.ResponseWriter, r *http.Request) {
@@ -43,7 +58,7 @@ func handleRoutes() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/gettasks", getTasks).Methods("GET")
-	router.HandleFunc("/gettask", getTask).Methods("GET")
+	router.HandleFunc("/gettask/{id}", getTask).Methods("GET")
 	router.HandleFunc("/create", createTask).Methods("POST")
 	router.HandleFunc("/delete/{id}", deletTask).Methods("DELETE")
 	router.HandleFunc("/update/{id}", updateTask).Methods("PUT")
@@ -51,5 +66,7 @@ func handleRoutes() {
 }
 
 func main() {
+	tasks = append(tasks, Task{ID: "1", Name: "first", Detail: "first detail", Date: "2023-08-19"})
+	tasks = append(tasks, Task{ID: "2", Name: "second", Detail: "second detail", Date: "2023-08-19"})
 	handleRoutes()
 }
